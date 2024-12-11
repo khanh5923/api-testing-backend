b40d734ad34dc44aaa786c0e08414cd19d672225
Phong Do <dominhphong306@gmail.com>
2024-12-12 06:15:20 +0700

    feat: move repo

--- src/books/dto/borrow-book.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
-
-export class BorrowBookDto {
-  @ApiProperty({ required: false, description: 'Optional notes about the borrowing' })
-  @IsOptional()
-  @IsString()
-  notes?: string;
-}
--- src/config/typeorm.config.ts
+++
@@ -1,9 +0,0 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { join } from 'path';
-
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: 'library.db',
-  entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
-  synchronize: true, // Set to false in production
-};
